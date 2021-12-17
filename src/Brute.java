771ad109a0d9bb1d713fc8d3b170a02c86f8c5f0
hakhanhne <s3877707@rmit.edu.vn>
2021-12-17 11:58:14 +0700

    Path with max gold but minimum step

--- src/Brute.java
+++ src/Brute.java
@@ -1,28 +1,32 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Brute {
     private static char[][] maze = null;
     private static int n, m, maxGold = Integer.MIN_VALUE;
-    private static int currentCoin = 0;
-    private static String str = "";
+    private static String finalPath = "";
     private static int[][] cor = new int[50][50];
+
+    // read maze from file
     public static void read(String filename) {
-        File file = new File(filename);
-        if (!file.exists()) {
+        File file = new File(filename); // attempt to open file
+        if (!file.exists()) { // check if file exists/can be opened or not
+            System.out.println("error opening file"); // print message if cant open file
             return;
         }
+        // init scanner
         Scanner scanner = null;
         try {
             scanner = new Scanner(file);
         } catch (Exception e) {
             System.out.println(e);
         }
-        assert scanner != null;
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-        maze = new char[n][m];
+        assert scanner != null; // assume scanner not null. if assumption not true, system will throw an error
+        n = scanner.nextInt(); // get the number of rows
+        m = scanner.nextInt(); // get the number of columns
+        maze = new char[n][m]; // create a matrix with n rows and m columns
+
+        // fill value to matrix
         scanner.nextLine();
         for (int i = 0; i < n; i++) {
             String line = scanner.nextLine();
@@ -30,37 +34,55 @@ public class Brute {
                 maze[i][j] = line.charAt(j);
             }
         }
-
     }
-    public static void dfs(int i, int j, int goldCollected) {
+
+    public static void dfs(int i, int j, int goldCollected, StringBuilder currentPath, String direction) {
+        // check if this position is valid to go to or not, if not then return
         if(i<0 || i>=n || j<0 || j>=m || maze[i][j] == 'X') {
             return;
         }
-        int currentCoin;
-        if(maze[i][j] == '.')
+
+        // if can go then update current path
+        currentPath.append(direction);
+
+        int currentCoin; // to get the number of golds at this location
+        if(maze[i][j] == '.') // if land then there's no gold
             currentCoin = 0;
         else
-            currentCoin = maze[i][j] - '0';
+            currentCoin = maze[i][j] - '0';  // if gold then assign value (int) to current gold
 //        //debug
 //        maze[i][j] = 'X';
-        maxGold = Math.max(maxGold, goldCollected + currentCoin);
 
-        dfs(i+1,j,goldCollected + currentCoin);
-        dfs(i,j+1,goldCollected + currentCoin);
-        maze[i][j] = (char) (currentCoin + '0');
+        // check if current collected golds is bigger than current maxGold
+        // if true then update maxGold and finalPath
+        // also, if it equals maxGold but has shorter path, then update finalPath
+        if (maxGold < (goldCollected + currentCoin) || (maxGold == (goldCollected+currentCoin) && (finalPath.length() > currentPath.length()))) {
+            maxGold = goldCollected + currentCoin;
+            finalPath = currentPath.toString();
+        }
+
+        // go down, same column
+        dfs(i+1,j,goldCollected + currentCoin, currentPath,"D");
+        // go right, same row
+        dfs(i,j+1,goldCollected + currentCoin, currentPath,"R");
+        maze[i][j] = (char) (currentCoin + '0'); // dòng này để chi z Tuấn Anh?
+
+        // remove the last element in currentPath when return
+        if (currentPath.length() > 0)
+            currentPath.deleteCharAt(currentPath.length()-1);
     }
 
     public static int solve() {
-        dfs(0,0,0);
+        dfs(0,0,0, new StringBuilder(),"");
         return maxGold;
-
     }
 
 
     public static void main(String[] args) {
         if (args.length != 1) return;
         read(args[0]);
-        System.out.println(solve());
-        System.out.println(str);
+        System.out.println("Golds = " + solve());
+        System.out.println("Steps = " + finalPath.length());
+        System.out.println("Path: " + finalPath);
     }
 }
