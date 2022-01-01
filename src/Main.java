254de2d3aa853f84faacd8299a86ecbe8bbb5e4d
RenuzitV <nvminhduy2002@gmail.com>
2022-01-01 23:42:02 +0700

    fixed code to read shit seperated by spaces and add exceptions to input

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import javafx.util.Pair;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
-    private static final char[][] maze = new char[25][25];
+    private static final String[][] maze = new String[25][25];
     //a pair consists of a key (coin collected) and a value (length of path)
     private static final Pair<Integer, Integer>[][] dp = new Pair[25][25];
     //this pair contains the parent location that leads to this location being the most optimal path
@@ -26,21 +28,41 @@ public class Main {
             return; //exit function if there is no file to be read
         }
 
-        n = scanner.nextInt(); // get the number of rows
-        m = scanner.nextInt(); // get the number of columns
-        scanner.nextLine(); //remove \n character
+        try {
+            n = scanner.nextInt(); // get the number of rows
+            m = scanner.nextInt(); // get the number of columns
+            scanner.nextLine(); //remove \n character
+        } catch (Exception e){
+            System.out.println("Error : cannot read number of rows or columns");
+            return;
+        }
 
-        for (int i = 1; i <= n; ++i){
-            maze[i] = (" " + scanner.nextLine() + " ").toCharArray(); //read maze in, and offsets the maze to start at (1, 1) and ends at (n, m)
+        for (int i = 1; i <= n; ++i) {
+            String[] s;
+            try {
+                //reads the next line, and splits them into strings seperated by spaces
+                s = scanner.nextLine().split("\\s+");
+            } catch (Exception e){
+                System.out.println("Error : cannot read line " + i);
+                return;
+            }
+            if (s.length != m) {
+                System.out.println("Error : line " + i + " does not have " + m + " elements seperated by spaces.");
+                return;
+            }
+            for (int j = 1; j <= m; ++j){
+                if (s[j-1].equals(".")) maze[i][j] = "0";
+                else maze[i][j] = s[j-1]; //initialize '.' as '0' for convenience
+            }
         }
 
         //initialize dynamic programming matrix
         for (int i = 0; i <= n+1; ++i){
             for (int j = 0; j <= m+1; ++j) {
                 dp[i][j] = new Pair<>(-500, 0); //anything that is not the root is initially unreachable, and therefore has -500 weighting
-                if (maze[i][j] == '.') maze[i][j] = '0'; //initialize '.' as '0' for convenience
             }
         }
+
         //the idea of -500 is to make sure whatever optimal answer we can get from "unreachable nodes" (nodes that cannot
         //be traversed from (1, 1)) is always worse than if we did not move at all.
         //the most we can get from such an answer is to move 25 moves down and 24 moves right, with 9 points each move, which is 449, and we round it up to 500 for simplicity.
@@ -66,9 +88,9 @@ public class Main {
         for (int i = 1; i <= n; ++i){
             for (int j = 1; j <= m; ++j){
                 //if the current node is not 'X'
-                if (maze[i][j] != 'X'){
+                if (!maze[i][j].equals("X")){
                     //make a pair of (coins, length of path for this node)
-                    Pair<Integer, Integer> temp = add(dp[i - 1][j], new Pair<>(maze[i][j] - '0', 1));
+                    Pair<Integer, Integer> temp = add(dp[i - 1][j], new Pair<>(Integer.valueOf(maze[i][j]), 1));
                     //if the result at this node is either reachable i.e. (0, 0) vs (-1, 0) or just better i.e. (12, 4) vs (15,6) then we update the current node
                     if (better(temp, dp[i][j])) {
                         dp[i][j] = temp; //update result
@@ -76,7 +98,7 @@ public class Main {
                     }
 
                     //same thing as above, but from the left
-                    temp = add(dp[i][j-1], new Pair<>(maze[i][j] - '0', 1));
+                    temp = add(dp[i][j-1], new Pair<>(Integer.valueOf(maze[i][j]), 1));
                     if (better(temp, dp[i][j])){
                         dp[i][j] = temp;
                         parent[i][j] = new Pair<>(i, j - 1);
